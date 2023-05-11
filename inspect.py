6216f5741e860430daabf716428f2ccc6f2c0b6d
Alex Beltran <alexbeltran@berkeley.edu>
2023-05-11 02:21:49 -0700

    add dummy tmp path for dag testing

--- inspect.py
+++ inspect.py
@@ -1,3 +1,3 @@
 import json
 with open("/airflow/xcom/return.json", "w") as f:
-    json.dump({"MEDIA_PATH": "", "STREAM_INDEX": "0", "AUDIO_CHANNEL": "0"}, f)
+    json.dump({"MEDIA_PATH": "/tmp/tmp.tmp", "STREAM_INDEX": "0", "AUDIO_CHANNEL": "0"}, f)
