3ee832ccdfe2de4a41f822a6f1a7467b92369069
Alex Beltran <alexbeltran@berkeley.edu>
2023-05-11 03:50:43 -0700

    passthrough media_path

--- inspect.py
+++ inspect.py
@@ -1,3 +1,6 @@
 import json
+import os
+
+media_path = os.environ.get("MEDIA_PATH")
 with open("/airflow/xcom/return.json", "w") as f:
-    json.dump({"MEDIA_PATH": "/tmp/tmp.tmp", "STREAM_INDEX": "0", "AUDIO_CHANNEL": "0"}, f)
+    json.dump({"MEDIA_PATH": media_path, "STREAM_INDEX": "0", "AUDIO_CHANNEL": "0"}, f) 
