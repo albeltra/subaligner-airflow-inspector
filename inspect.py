7a41898c4a114b37c46cbe1a24704bcce87def64
Alex Beltran <alex@meth.local>
2023-05-23 02:43:22 -0700

    restructure for arr data

--- inspect.py
+++ inspect.py
@@ -1,6 +1,14 @@
 import json
 import os
 
-media_path = os.environ.get("MEDIA_PATH")
-with open("/airflow/xcom/return.json", "w") as f:
-    json.dump({"MEDIA_PATH": media_path, "STREAM_INDEX": "0", "AUDIO_CHANNEL": "0"}, f) 
+mediaFile = os.environ.get("mediaFile")
+mediaInfo = os.environ.get("mediaInfo")
+stream_index = os.environ.get("stream_index", "0")
+audio_channel = os.environ.get("audio_channel", "0")
+
+if mediaInfo and mediaFile:
+    media_path = mediaFile.get("path")
+
+if media_path is not None:
+    with open("/airflow/xcom/return.json", "w") as f:
+        json.dump({"MEDIA_PATH": media_path, "STREAM_INDEX": stream_index, "AUDIO_CHANNEL": audio_channel}, f)
